b7c2816833170f1e4b7aa27cd049a5769557730e
Fernando Chavez <fjchavez01@gmail.com>
2020-10-19 13:17:21 -0700

    added basic form for the sign up page

--- schoolinthecloud/src/App.js
+++ schoolinthecloud/src/App.js
@@ -3,20 +3,27 @@ import { Jumbotron, Button } from 'reactstrap';
 import './App.css';
 import ClassForm from './ClassForm'
 import Signup from './components/Signup'
+import {Route} from 'react-router-dom'
 
 function App() {
   return (
     <div>
-      <Jumbotron>
-        <h1 className="display-3">School in the Cloud</h1>
-        <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
-        <hr className="my-2" />
-        <p>School in the Cloud is a platform that trains senior volunteers to teach students in a group or individual setting. This helps kids in communities with high student to teacher ratios. It also provides retired volunteers a sense of purpose and meaning in their day to day life when they find themselves with more free time. The platform also connects volunteers with the students.</p>
-        <p className="lead">
-          <Button>Display Classes</Button>
-          <ClassForm />
-        </p>
-      </Jumbotron>
+      <Route exact path='/'>
+        <Jumbotron>
+          <h1 className="display-3">School in the Cloud</h1>
+          <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
+          <hr className="my-2" />
+          <p>School in the Cloud is a platform that trains senior volunteers to teach students in a group or individual setting. This helps kids in communities with high student to teacher ratios. It also provides retired volunteers a sense of purpose and meaning in their day to day life when they find themselves with more free time. The platform also connects volunteers with the students.</p>
+          <p className="lead">
+            <Button>Display Classes</Button>
+            <ClassForm />
+          </p>
+        </Jumbotron>
+      </Route>
+
+      <Route path = '/signup'>
+        <Signup/>
+      </Route>
  
     </div>
   );
--- schoolinthecloud/src/components/Signup.js
+++ schoolinthecloud/src/components/Signup.js
@@ -1,10 +1,42 @@
-import React from 'react'
+import React, {useState} from 'react'
 import '../components/Signup.css'
 
 export default function Signup(){
+    const initialFormValues = {
+        firstName: '',
+        lastName: '',
+        password: '',
+        confirmPassword: '',
+        email: '',
+        role: '',
+    }
+    const [errors, setErrors] = useState(initialFormValues)
+    const [formValues, setFormvalues] = useState(initialFormValues)
     return(
         <div className = 'MainDiv'> 
-            <h1>Hello</h1>
+            <h1 className='cloud'>Cloud School</h1>
+            <from className='form'>
+                <label className='label'>First Name</label>
+                <input type='text' name='firstName'/>
+                <label className='label'>Last Name</label>
+                <input type='text' name='lastName'/>
+                <label className='label'>Email</label>
+                <input type='text' name='email'/>
+                <label className='label'>Role</label>
+                <select name='role'>
+                    <option value=''>---Select Role---</option>
+                    <option value='student'>Student</option>
+                    <option value='volunteer'>Volunteer</option>
+                </select>
+                <label className='label'>Password</label>
+                <input type='password' name='password'/>
+                <label className='label'>Confirm Password</label>
+                <input type='password' name='confirmPassword'/>
+                <label className='label'>
+                    <input type='submit'/>
+                </label>
+
+            </from>
         </div>
     )
 }
