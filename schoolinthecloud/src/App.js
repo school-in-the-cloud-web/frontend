5592b730f05615ed26a1b602da8e165689917ec2
Sam Tarullo <sam@Sams-Air.attlocal.net>
2020-10-19 20:10:45 -0500

    set up redux

--- schoolinthecloud/src/App.js
+++ schoolinthecloud/src/App.js
@@ -1,20 +1,45 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { Jumbotron, Button } from 'reactstrap';
 import AdminDashboard from './AdminDashboard';
 import './App.css';
-import ClassForm from './ClassForm'
+import ClassForm from './ClassForm';
+import PrivateRoute from './utils/PrivateRoute';
+
+import {Link} from 'react-router-dom';
+
+
+import axios from 'axios';
 
 function App() {
+  const [classes, setClasses] = useState(null)
+  useEffect(()=>{
+    axios.post('https://cloud-school-api.herokuapp.com/auth/login', {email: "starullo@email.com", password: "gRh06ZYT1gSB"})
+    .then(res=>{
+      console.log(res);
+      window.localStorage.setItem('token', res.data.token);
+    })
+    .catch(err => {
+      console.log(err.response)
+    })
+    // axios.get('https://cloud-school-api.herokuapp.com/tasks')
+    // .then(res=>{
+    //   console.log(res)
+    // })
+    // .catch(err=>{
+    //   console.log(err)
+    // })
+  }, [])
   return (
     <div>
       <Jumbotron>
+        <Link to='/admin-dashboard'>DASHBOARD</Link>
         <div className="jumbotron">
           <h1 className="display-3">School in the Cloud</h1>
           <p>School in the Cloud is a platform that trains senior volunteers to teach students in a group or individual setting. This helps kids in communities with high student to teacher ratios. It also provides retired volunteers a sense of purpose and meaning in their day to day life when they find themselves with more free time. The platform also connects volunteers with the students.</p>
         </div>
           {/* <Button>Display Classes</Button> */}
           <hr />
-          <AdminDashboard />
+          <PrivateRoute exact path='/admin-dashboard'component={AdminDashboard} />
       </Jumbotron>
     </div>
   );
---
+++ schoolinthecloud/src/utils/axiosWithAuth.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+export const axiosWithAuth = () => {
+    const token = window.localStorage.getItem('token');
+
+    return axios.create({
+        headers: {
+            Authorization: token,
+        },
+        baseURL: 'https://cloud-school-api.herokuapp.com/'
+    })
+}
